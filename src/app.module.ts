e5fc396e4a91b7ebe043fab5a22563d74bf3afdd
Fernando <fer3443@gmail.com>
2025-07-07 11:58:53 -0300

    feat:preparacion de mongodb y env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,32 @@
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfigutarion } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load:[EnvConfigutarion],
+      validationSchema: JoiValidationSchema
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB as string, {
+      dbName:'pokemondb'
+    }),
     PokemonModule,
     CommonModule,
     SeedModule
   ],
 })
-export class AppModule { }
+export class AppModule {
+
+}
